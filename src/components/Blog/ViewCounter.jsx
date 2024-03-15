2fde372edfcc9f7d0b78d48b10fd8a0bb4ee9567
wanderson.lourenco <wanderson.lourenco@wises.com.br>
2024-03-15 11:56:31 -0400

    Removido supabase

--- src/components/Blog/ViewCounter.jsx
+++ src/components/Blog/ViewCounter.jsx
@@ -1,31 +1,31 @@
 'use client';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+//import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import React, { useEffect, useState } from 'react';
 
-const supabase = createClientComponentClient();
+//const supabase = createClientComponentClient();
 
 const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
   const [views, setViews] = useState(0);
 
   useEffect(() => {
     const incrementView = async () => {
-      try {
-        let { error } = await supabase.rpc('increment', {
-          slug_text: slug
-        });
+      // try {
+      //   let { error } = await supabase.rpc('increment', {
+      //     slug_text: slug
+      //   });
 
-        if (error) {
-          console.error(
-            'Error incrementing view count inside try block:',
-            error
-          );
-        }
-      } catch (error) {
-        console.error(
-          'An error occurred while incrementing the view count:',
-          error
-        );
-      }
+      //   if (error) {
+      //     console.error(
+      //       'Error incrementing view count inside try block:',
+      //       error
+      //     );
+      //   }
+      // } catch (error) {
+      //   console.error(
+      //     'An error occurred while incrementing the view count:',
+      //     error
+      //   );
+      // }
     };
 
     if (!noCount) {
